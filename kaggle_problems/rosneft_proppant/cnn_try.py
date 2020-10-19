996f5a11eca9c020cee204140236cd2015150add
skrrydg <skrrydg@yandex.ru>
2020-10-19 12:51:58 +0300

    black white image processing

--- kaggle_problems/rosneft_proppant/cnn_try.py
+++ kaggle_problems/rosneft_proppant/cnn_try.py
@@ -42,28 +42,14 @@ from kaggle_problems.rosneft_proppant.workspace.common import TARGET_SHAPE
 # In[3]:
 
 
-DATA_DIR = "kaggle_problems/rosneft_proppant/workspace/data/"
-MODEL_DIR = "kaggle_problems/rosneft_proppant/workspace/models"
-DF_RATE = 1. / 15
+DATA_DIR = "kaggle_problems/rosneft_proppant/data/"
+MODEL_DIR = "kaggle_problems/rosneft_proppant/worace/models"
+GENERATED_DIR = "kaggle_problems/rosneft_proppant/data/generated/"
+GENERATED_IMG_DIR = GENERATED_DIR + "colored_img"
+GENERATED_LABELS_DIR = GENERATED_DIR + "labels"
+DF_RATE = 1.
 
-
-# In[4]:
-
-
-train = pd.read_csv("{}labels/labels.csv".format(DATA_DIR))
-train.describe()
-
-
-# In[ ]:
-
-
-
-
-
-# In[5]:
-
-
-sources = ['bw', 'colored', 'threshed']
+sources = ['colored']
 source_to_fraction = {
     'bw': 'bw',
     'colored': 'colored',
@@ -71,7 +57,8 @@ source_to_fraction = {
 }
 
 
-# In[6]:
+# In[15]:
+
 
 
 def enrich_fraction(train):
@@ -80,49 +67,53 @@ def enrich_fraction(train):
         train.loc[train.ImageId.isin(img_numbers), 'fraction'] = fraction
     return train
 
-def get_fraction_sievs(train, fraction):
-    train_fraction = train[train.fraction == fraction]
+def get_fraction_sievs(data, fraction):
+    data_fraction = data[data.fraction == fraction]
     result_bins = []
     for b in bins:
-        if train_fraction[b].sum() > 1e-5:
+        if data_fraction[b].sum() > 1e-5:
             result_bins.append(b)
     return result_bins
         
 
+def common_df_processing(data):
+    for i in bins:
+        if i in data.columns:
+            data = data[~data[i].isnull()]
 
-# In[7]:
+    data["filename"] = data['ImageId'].astype(str) + '.jpg'
+    return data
+
+def get_validation(source):
+    validation = pd.read_csv("{}labels/train.csv".format(DATA_DIR))
+    
+    validation.fraction = None
+    validation = enrich_fraction(validation)
+
+    validation = validation[~validation.fraction.isnull()]
+    
+    fraction = source_to_fraction[source]
+    validation = validation[validation['fraction'] == source_to_fraction[source]]
+
+    validation = common_df_processing(validation)
+    return validation
+
+def get_train(source):
+    train = pd.read_csv("{}/generated_{}_train.csv".format(GENERATED_LABELS_DIR, source))
+    
+    train = common_df_processing(train)
+    return train
 
 
-train.fraction = None
-train = enrich_fraction(train)
-
-
-# In[8]:
-
-
-train = train[~train.fraction.isnull()]
-
-
-# In[9]:
-
-
-for i in bins:
-    train = train[~train[i].isnull()]
-
-train["filename"] = train['ImageId'].astype(str) + '.jpg'
-
-
-# In[10]:
+# In[16]:
 
 
 fraction_sievs = {}
-for fraction in source_to_fraction.values():
-    fraction_sievs[fraction] = get_fraction_sievs(train, fraction)
 
 
 # ### Model
 
-# In[11]:
+# In[17]:
 
 
 class BinsExtraction(Model):
@@ -163,22 +154,52 @@ class BinsExtraction(Model):
 
 # #### Train Input Generator
 
-# In[12]:
+# In[18]:
 
 
-def get_train_val_datagen(train, source, train_size=0.8):
+# def get_train_val_datagen(train, source, train_size=0.8):
+#     fraction = source_to_fraction[source]
+#     train_fraction = train[train['fraction'] == source_to_fraction[source]]
+    
+#     train_fraction, val_fraction = train_test_split(train_fraction, train_size=train_size, random_state=123)
+    
+#     bins_fraction = fraction_sievs[fraction]
+    
+#     datagen = ImageDataGenerator()
+
+#     train_generator = datagen.flow_from_dataframe(
+#             train_fraction.sample(n=int(len(train_fraction) * DF_RATE)),
+#             directory="kaggle_problems/rosneft_proppant/data/{}_main_area".format(source),
+#             x_col='filename', 
+#             y_col=bins_fraction,
+#             target_size=TARGET_SHAPE,
+#             batch_size=16,
+#             class_mode='other',
+#     )
+    
+#     val_generator = datagen.flow_from_dataframe(
+#         val_fraction.sample(n=int(len(val_fraction) * DF_RATE)),
+#         directory="kaggle_problems/rosneft_proppant/data/{}_main_area".format(source),
+#         x_col='filename', 
+#         y_col=bins_fraction,
+#         target_size=TARGET_SHAPE,
+#         batch_size=16,
+#         class_mode='other')
+#     return train_generator, val_generator
+
+
+# In[19]:
+
+
+def get_train_val_datagen(train, validation, source):
     fraction = source_to_fraction[source]
-    train_fraction = train[train['fraction'] == source_to_fraction[source]]
-    
-    train_fraction, val_fraction = train_test_split(train_fraction, train_size=train_size, random_state=123)
-    
     bins_fraction = fraction_sievs[fraction]
     
     datagen = ImageDataGenerator()
 
     train_generator = datagen.flow_from_dataframe(
-            train_fraction.sample(n=int(len(train_fraction) * DF_RATE)),
-            directory="kaggle_problems/rosneft_proppant/workspace/data/{}_main_area".format(source),
+            train.sample(n=int(len(train) * DF_RATE)),
+            directory="kaggle_problems/rosneft_proppant/data/generated/{}_img".format(source),
             x_col='filename', 
             y_col=bins_fraction,
             target_size=TARGET_SHAPE,
@@ -186,35 +207,37 @@ def get_train_val_datagen(train, source, train_size=0.8):
             class_mode='other',
     )
     
-    val_generator = datagen.flow_from_dataframe(
-        val_fraction.sample(n=int(len(val_fraction) * DF_RATE)),
-        directory="kaggle_problems/rosneft_proppant/workspace/data/{}_main_area".format(source),
-        x_col='filename', 
-        y_col=bins_fraction,
-        target_size=TARGET_SHAPE,
-        batch_size=16,
-        class_mode='other')
-    return train_generator, val_generator
+    validation_generator = datagen.flow_from_dataframe(
+            validation.sample(n=int(len(validation) * DF_RATE)),
+            directory="kaggle_problems/rosneft_proppant/data/{}_main_area".format(source),
+            x_col='filename', 
+            y_col=bins_fraction,
+            target_size=TARGET_SHAPE,
+            batch_size=16,
+            class_mode='other',
+    )
+    
+    return train_generator, validation_generator
 
 
 # #### Input generator checking
 
-# In[13]:
+# In[20]:
 
 
-img, labels = get_train_val_datagen(train, 'bw')[0].next()
-display_images(img[0:8].astype(int), 4)
+# img, labels = get_train_val_datagen(train, validation, 'colored')[0].next()
+# display_images(img[0:8].astype(int), 4)
 
 
 # #### Callbacks
 
-# In[14]:
+# In[21]:
 
 
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 
-# In[15]:
+# In[22]:
 
 
 learning_rate_reduction = ReduceLROnPlateau(monitor='val_loss', 
@@ -228,25 +251,30 @@ earlystop = EarlyStopping(patience=10)
 callbacks = [earlystop, learning_rate_reduction]
 
 
-# In[16]:
+# In[23]:
 
 
 def metric(true, predicted):
     return tf.keras.backend.mean(tf.math.reduce_sum((true - predicted) ** 2 / (true + predicted), axis=1))
 
 
-# In[18]:
+# In[24]:
 
 
 for source, i in zip(sources, range(len(sources))):
+    validation = get_validation(source)
+    train = get_train(source)
     fraction = source_to_fraction[source]
+    
+    fraction_sievs[fraction] = get_fraction_sievs(validation, fraction)
+    
     model = BinsExtraction(fraction)
     model.compile(
         loss=metric,
         optimizer='rmsprop',
     )
 
-    train_datagen, val_datagen = get_train_val_datagen(train, source)
+    train_datagen, val_datagen = get_train_val_datagen(train, validation, source)
 
     history = model.fit(
         x=train_datagen,
@@ -282,28 +310,29 @@ def get_bins_metric_by_bins(predicted, true):
 #print("Total bin loss: {}".format(get_bins_metric(predicted_labels, all_labels)))
 
 
-# In[ ]:
+# In[14]:
 
 
 # for source, i in zip(sources, range(len(sources))):
-#     print(fraction + "-" * 50)
+#     fraction = source_to_fraction[source]
+#     print(source + "-" * 100)
 #     from keras.utils.generic_utils import get_custom_objects
 
 #     get_custom_objects().update({'metric': metric})
 
-#     with open(MODEL_DIR + "/history_model_benchmark_{}.pickle".format(i), 'rb') as f:
+#     with open(MODEL_DIR + "/history_model_benchmark_{}.pickle".format(source), 'rb') as f:
 #         history = pickle.load(f)
 
-#     model = tf.keras.models.load_model(MODEL_DIR + "/model_benchmark_{}".format(i), 
+#     model = tf.keras.models.load_model(MODEL_DIR + "/model_benchmark_{}".format(source), 
 #                                        compile=False)
 #     model.compile(
 #         loss=metric,
 #         optimizer='rmsprop',
-#         metrics=['mse']
+#        # metrics=['mse']
 #     )                                                                               
 
-#     display_history_metrics(history, fraction)
-#     print(fraction + '-' * 50)
+#     display_history_metrics(history, source)
+#     print(source + '-' * 100)
     
 #     train_datagen = get_train_val_datagen(train, fraction)[0]
 
@@ -332,6 +361,12 @@ def get_bins_metric_by_bins(predicted, true):
 
 
 get_ipython().system('jupyter nbconvert --to script kaggle_problems/rosneft_proppant/cnn_try.ipynb')
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
