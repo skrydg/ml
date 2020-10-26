197ce888cab63988d953990fab76bd4e9f307e67
skrrydg <skrrydg@yandex.ru>
2020-10-26 18:12:33 +0300

    black white image processing

--- kaggle_problems/rosneft_proppant/workspace/common.py
+++ kaggle_problems/rosneft_proppant/workspace/common.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 COEF = 30
 
 MAGIC_COEFF = 0.78
@@ -34,3 +36,38 @@ TARGET_SHAPE = (round(INNER_SHAPE[0] * COEF), round(INNER_SHAPE[1] * COEF))
 
 def r2prop_size(r):
     return 2 * r / COEF * MAGIC_COEFF
+
+def prop_size2r(prop_size):
+    return prop_size * COEF / 2 / MAGIC_COEFF
+
+def sizes_to_sieves(sizes, sive_diam, sieves_names):
+    """
+    Распределяет предикты по ситам
+    """
+    sizes_ = np.sort(sizes)
+    sieve_bins = np.zeros_like(sizes_)
+
+    for diam, name in zip(sive_diam, sieves_names):
+        sieve_bins[sizes_<= diam] = name
+
+    return sizes_, sieve_bins
+
+
+def generate_low_high(bins_name):
+    bin2low = {b: [] for b in bins_name}
+    bin2high = {b: [] for b in bins_name}
+    for r in range(1, 100):
+        sieves, names = sizes_to_sieves([r2prop_size(r)], [bins2mm[b] for b in bins_name], bins_name)
+        assert (len(names) == 1)
+        if int(names[0]) == 0:
+            continue
+        bin2low[str(int(names[0]))].append(r)
+        bin2high[str(int(names[0]))].append(r)
+
+
+    bin2low = {k: min(v if len(v) else [None]) for k, v in bin2low.items()}
+    bin2high = {k: max(v if len(v) else [None]) for k, v in bin2high.items()}
+
+    return bin2low, bin2high
+
+bin2low, bin2high = generate_low_high(bins)
